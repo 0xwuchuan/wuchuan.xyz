84574d390d9de5260e78c1681e74c29b0020a802
wuchuan <wuchuan@skiff.com>
2023-07-27 18:51:00 +0800

    chore: removed header + installed shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image'
-import Header from '@/components/header'
 
 export default function Home() {
   return (
-    <div><Header></Header></div>
+    <div></div>
   )
 }
--- src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <div className="fixed flex w-full z-[9999] items-center justify-center ">
-      <div className="mt-4 flex w-11/12 gap-6 rounded-2xl border border-solid border-black bg-[#0B0B0B] p-1 shadow-xl md:w-fit">
-        <div className="flex items-center justify-between w-full gap-0 h-9 md:justify-start "></div>
-      </div>
-    </div>
-  );
-}
