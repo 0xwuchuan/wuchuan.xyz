18b3c30404858ec1472676d93688f68a33758864
wuchuan <wuchuan@skiff.com>
2023-07-27 22:00:53 +0800

    feat:worked on footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Header from '@/components/header'
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div className="max-w-2xl px-10 grow">
       <Header />
+      <p className="pb-80">
+        Currently exploring different fields of software engineering,
+        particularly blockchain, web3 and full-stack development and am actively
+        seeking internship opportunities in these spaces
+      </p>
+      <Footer />
     </div>
-    
-  )
+  );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,16 @@
+import { Separator } from "./ui/separator";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Separator className="my-5 opacity-50 bg-neutral-700" />
+      <div className="flex justify-between">
+        <p className="px-2 text-sm text-neutral-300">contact me</p>
+        {/* For all contact methods */}
+        <div>
+          <p className="px-2 text-sm text-neutral-300">email</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
