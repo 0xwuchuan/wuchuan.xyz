64e0212d64edd6e3fb41dd471b60bd0ab5d99557
wuchuan <wuchuan@skiff.com>
2024-04-26 19:49:57 +0800

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,36 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'khoowuchuan',
-}
+  title: {
+    template: "wuchuan • %s",
+    default: "wuchuan • home",
+  },
+  icons: {
+    icon: "/icon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       {/* Standard layout: Center children vertically in one column */}
-      <body className={"flex flex-col items-center min-h-screen overflow-x-hidden" + inter.className}>{children}</body>
+      <body
+        className={
+          "flex flex-col items-center min-h-screen overflow-x-hidden" +
+          inter.className
+        }
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
